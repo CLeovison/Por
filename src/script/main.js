03fed742fbdea23ebba05c8f3debd4a7ffa6d7b6
CLeovison <cleovison@gmail.com>
2023-09-01 10:27:46 +0800

    Added New Image File

---
+++ src/script/main.js
@@ -0,0 +1,7 @@
+const imgContainer = document.querySelector('.img-container');
+const openContainer = document.querySelectorAll('.img-container > img');
+const openModal = document.querySelector('.open-modal');
+const hiddenModal = document.querySelector('.hidden-modal');
+const alt = document.querySelector('.open-modal > h3');
+
+
