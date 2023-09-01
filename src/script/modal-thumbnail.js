fb5bae951aa6d7e6dbcbbd02d79ab0c52a0df1bf
CLeovison <cleovison@gmail.com>
2023-09-01 14:09:48 +0800

    Changes

---
+++ src/script/modal-thumbnail.js
@@ -0,0 +1,8 @@
+const imgContainer = document.querySelectorAll('.img-container');
+
+const openModal = document.querySelector('.open-modal');
+const hiddenModal = document.querySelector('.hidden-modal');
+const alt = document.querySelector('.open-modal > h3');
+
+
+imgContainer.forEach()
