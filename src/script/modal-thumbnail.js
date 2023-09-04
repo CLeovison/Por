f0158aae2fe3822d3e1c04f94a80feffd1a5bc67
CLeovison <cleovison@gmail.com>
2023-09-04 17:59:37 +0800

    Removed JS File

--- src/script/modal-thumbnail.js
+++
@@ -1,8 +0,0 @@
-const imgContainer = document.querySelectorAll('.img-container');
-
-const openModal = document.querySelector('.open-modal');
-const hiddenModal = document.querySelector('.hidden-modal');
-const alt = document.querySelector('.open-modal > h3');
-
-
-imgContainer.forEach()
