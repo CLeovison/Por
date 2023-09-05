08d4d9f3f37be32a45e981b23a83ed729c5ece34
CLeovison <cleovison@gmail.com>
2023-09-05 16:06:40 +0800

    Light And Dark

---
+++ src/script/main.js
@@ -0,0 +1,6 @@
+//Light & Dark Mode Section
+
+const lightDark = document.querySelectorAll('.light-dark');
+let lightTheme = document.querySelector('.light-theme');
+let darkTheme = document.querySelector('.dark-theme');
+
