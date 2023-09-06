cf18cc08e4069f1f6dce0b18c5dccd0ad8f50958
CLeovison <cleovison@gmail.com>
2023-09-06 18:13:56 +0800

    Fixed Light/Dark

--- src/script/main.js
+++ src/script/main.js
@@ -1,6 +1,20 @@
 //Light & Dark Mode Section
 
-const lightDark = document.querySelectorAll('.light-dark');
-let lightTheme = document.querySelector('.light-theme');
-let darkTheme = document.querySelector('.dark-theme');
+const lightDark = document.querySelector('.light-dark');
+let theme = document.querySelector('.light-dark > .theme')
 
+
+
+lightDark.addEventListener('click', ()=>{
+    //theme.classList.toggle('fa-moon')
+
+   if(theme.classList.contains('fa-moon')){
+    document.body.classList.add('active')
+    theme.classList.remove('fa-moon')
+    theme.classList.add('fa-sun')
+   }else{
+    theme.classList.remove('fa-sun')
+    theme.classList.add('fa-moon')
+    document.body.classList.remove('active');
+   }
+})
