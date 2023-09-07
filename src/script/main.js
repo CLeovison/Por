e75584d833576724e9a115bbb3eb6c8c947953df
CLeovison <cleovison@gmail.com>
2023-09-07 17:06:46 +0800

    Added Styles on Javascript

--- src/script/main.js
+++ src/script/main.js
@@ -1,39 +1,37 @@
 //Light & Dark Mode Section
 
-const lightDark = document.querySelector('.light-dark');
-let theme = document.querySelector('.light-dark > .theme')
-let storage = localStorage.getItem('themes');
-const trial = document.querySelector('.trial')
+const lightDark = document.querySelector(".light-dark");
+let theme = document.querySelector(".light-dark > .theme");
+let storage = localStorage.getItem("themes");
+const trial = document.querySelector(".trial");
 
-function setLight(){
-    document.body.classList.add('active')
-    theme.classList.remove('fa-moon')
-    theme.classList.add('fa-sun')
-    trial.classList.add('active')
-    document.querySelector('.light-dark > .theme').style.color = 'black';
+function setLight() {
+  document.body.classList.add("active");
+  theme.classList.remove("fa-moon");
+  theme.classList.add("fa-sun");
+  trial.classList.add("active");
+  document.querySelector(".light-dark > .theme").style.color = "black";
 }
-function setDark (){
-    theme.classList.remove('fa-sun')
-    theme.classList.add('fa-moon')
-    document.body.classList.remove('active');
-    trial.classList.remove('active')
-    document.querySelector('.light-dark > .theme').style.color = 'white';
+function setDark() {
+  theme.classList.remove("fa-sun");
+  theme.classList.add("fa-moon");
+  document.body.classList.remove("active");
+  trial.classList.remove("active");
+  document.querySelector(".light-dark > .theme").style.color = "white";
 }
 
-if(storage === 'light'){
+if (storage === "light") {
+  setLight();
+} else {
+  setDark();
+}
+
+lightDark.addEventListener("click", () => {
+  if (theme.classList.contains("fa-moon")) {
     setLight();
-}else{
+    localStorage.setItem("themes", "light");
+  } else {
     setDark();
-}
-
-lightDark.addEventListener('click', ()=>{
-
-   if(theme.classList.contains('fa-moon')){
-    setLight();
-    localStorage.setItem('themes', 'light')
-   }else{
-    setDark();
-    localStorage.setItem('themes', 'dark')
-   }
-})
-
+    localStorage.setItem("themes", "dark");
+  }
+});
