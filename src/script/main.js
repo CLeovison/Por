4085f809ad9751ae331ff7daaeb1959de3c541d9
CLeovison <cleovison@gmail.com>
2023-09-07 15:10:02 +0800

    Fixed Light/Dark Toggle and Addition of New Nav

--- src/script/main.js
+++ src/script/main.js
@@ -2,19 +2,35 @@
 
 const lightDark = document.querySelector('.light-dark');
 let theme = document.querySelector('.light-dark > .theme')
+let storage = localStorage.getItem('themes');
+const trial = document.querySelector('.trial')
 
-
-
-lightDark.addEventListener('click', ()=>{
-    //theme.classList.toggle('fa-moon')
-
-   if(theme.classList.contains('fa-moon')){
+function setLight(){
     document.body.classList.add('active')
     theme.classList.remove('fa-moon')
     theme.classList.add('fa-sun')
-   }else{
+    trial.classList.add('active')
+}
+function setDark (){
     theme.classList.remove('fa-sun')
     theme.classList.add('fa-moon')
     document.body.classList.remove('active');
+    trial.classList.remove('active')
+}
+
+if(storage === 'light'){
+    setLight();
+}else{
+    setDark();
+}
+
+lightDark.addEventListener('click', ()=>{
+
+   if(theme.classList.contains('fa-moon')){
+    setLight();
+    localStorage.setItem('themes', 'light')
+   }else{
+    setDark();
+    localStorage.setItem('themes', 'dark')
    }
 })
