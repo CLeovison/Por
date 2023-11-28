89eb48324088f31b0358090b95dd214f46c91b9c
Leovison <cleovison@gmail.com>
2023-11-28 18:24:06 +0800

    Added a Window Event Listner

--- src/script/main.js
+++ src/script/main.js
@@ -63,34 +63,18 @@ window.addEventListener("scroll", () => {
 });
 
 //Intersection Observer
-
 function createObserver() {
-  let options = {
-    root: document.querySelector("#scrollArea"),
-    rootMargin: "0px",
-    threshold: 1.0,
-  };
 
-  let observer = new IntersectionObserver(callback, options);
-
-  let target = document.querySelector("#listItem");
-  observer.observe(target);
-
-  let callback = (entries, observer) => {
-    entries.forEach((entry) => {
-      // Each entry describes an intersection change for one observed
-      // target element:
-      //   entry.boundingClientRect
-      //   entry.intersectionRatio
-      //   entry.intersectionRect
-      //   entry.isIntersecting
-      //   entry.rootBounds
-      //   entry.target
-      //   entry.time
-    });
-  };
 }
-createObserver();
+
+
+window.addEventListener('load', ()=>{
+  createObserver();
+})
+
+
+
+
 
 //Documentation//
 
